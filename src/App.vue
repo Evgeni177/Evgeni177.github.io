20e2937be4e67122d1b03d9f06bc96afdfe6fa0a
Evgeni998 <evgenikunchev98@gmail.com>
2021-03-11 08:56:15 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="main-container">
+            <div class="sidebar"> Sidebar </div>
+            <div class="main">
+                <div class="heading"> 
+                    <acasdascount-details v-if="state == 1">
+                        <h2 slot="name">Evgeni Kunchev</h2>
+                    </acasdascount-details>
+
+                </div>
+                <div class="center">
+                    <div class="left"> 2 </div>
+                    <div class="right"> 3 </div>
+                </div>
+                <div class="footer"> 4 </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import AccountDetails from './components/AccountDetails';
+// import CustomerDetails from './components/CustomerDetails';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        // AccountDetails,
+        // CustomerDetails
+    },
+    data() {
+        return {
+            state: 1
+        }
+    },
+    methods: {
+        buttonClicked() {
+            if(this.state == 1) {
+                this.state = 2;
+            } else {
+                this.state = 1;
+            }
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    html, body, #app {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        display: flex;
+        align-items: center;
+        background: rgb(252, 238, 222);
+    }
+    .main-container {
+        height: 95%;
+        width: 100%;
+        background: rgb(136, 136, 136);
+        display: flex;
+        padding: 10px;
+    }
+    .sidebar {
+        border: 1px solid black;
+        flex: 1;
+        margin: .1rem;
+    }
+    .main {
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        flex: 3;
+        margin: .1rem;
+    }
+    .main > div {
+        border: 1px solid black;
+        padding: .5rem;
+        margin: .3rem;
+    }
+    .heading {
+        flex: 1;
+    }
+    .center {
+        display: flex;
+        flex: 10;
+    }
+    .center > div {
+        border: 1px solid black;
+        padding: .5rem;
+        margin: .2rem;
+    }
+    .left {
+        flex: 3;
+    }
+    .right {
+        flex: 4;
+    }
+    .footer {
+        flex: 1;
+    }
 </style>
---
+++ src/components/CustomerDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot name="heading"></slot>
+        <slot name="paragraph"></slot>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'CustomerDetails'
+}
+</script>
+
+<style lang="css">
+
+</style>
